9dba76ed2b0a0631f59dd8f69be343b657b3cddc
Calum <calum.johnston.web@gmail.com>
2018-08-06 08:01:26 +1000

    IE

--- js/script.js
+++ js/script.js
@@ -1,6 +1,35 @@
-$(document).ready(function(){
-    $('.hero').tilt({
-        glare: true,
-        maxGlare: .5
-    });
-});
+
+/**
+ * detect IE
+ * returns version of IE or false, if browser is not Internet Explorer
+ */
+function detectIE() {
+var ua = window.navigator.userAgent;
+
+var msie = ua.indexOf("MSIE ");
+if (msie > 0) {
+    // IE 10 or older => return version number
+    return parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)), 10);
+}
+
+var trident = ua.indexOf("Trident/");
+if (trident > 0) {
+    // IE 11 => return version number
+    var rv = ua.indexOf("rv:");
+    return parseInt(ua.substring(rv + 3, ua.indexOf(".", rv)), 10);
+}
+
+var edge = ua.indexOf("Edge/");
+if (edge > 0) {
+    // Edge (IE 12+) => return version number
+    return parseInt(ua.substring(edge + 5, ua.indexOf(".", edge)), 10);
+}
+
+// other browser
+return false;
+}
+
+// If IE
+if (detectIE() !== false) {
+document.write("<style>ion-icon{display:none}.hero-content__highlight-title{margin-top:0.5em;}</style>");
+}
